459d4ef3fd1400fcf35565744c16010ee8f81278
Ahmad Imam Fadhila <ahmadimamfadhila@gmail.com>
2019-05-06 19:49:46 +0700

    Revert "Auto stash before revert of "inisialisasi""
    This reverts commit 5837906dde4183a3e094ceed13761ad1b40933be.

---
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/controller/RestaurantController.java
@@ -0,0 +1,5 @@
+package com.pbkk.fp.RestaurantService.controller;
+
+public class RestaurantController {
+
+}
---
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/service/RestaurantServImpl.java
@@ -0,0 +1,16 @@
+package com.pbkk.fp.RestaurantService.service;
+
+import java.util.List;
+
+import com.pbkk.fp.RestaurantService.model.RestaurantModel;
+
+public class RestaurantServImpl implements RestaurantServ{
+
+	@Override
+	public List<RestaurantModel> getAllRestaurant() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
