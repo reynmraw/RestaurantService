7cd1d7d3a42fd70d07b924b15e62aff4807d102e
Ahmad Imam Fadhila <ahmadimamfadhila@gmail.com>
2019-05-06 13:48:09 +0700

    inisialisasi

---
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/model/RestaurantModel.java
@@ -0,0 +1,16 @@
+package com.pbkk.fp.RestaurantService.model;
+
+public class RestaurantModel {
+
+	private Integer restaurantid;
+	
+	private String restaurantname;
+	
+	public Integer getRestaurantid(){
+		return restaurantid;
+	}
+	
+	public String getRestaurantname() {
+		return restaurantname;
+	}
+}
---
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/service/RestaurantServ.java
@@ -0,0 +1,9 @@
+package com.pbkk.fp.RestaurantService.service;
+
+import java.util.List;
+
+import com.pbkk.fp.RestaurantService.model.RestaurantModel;;
+
+public interface RestaurantServ {
+	public List<RestaurantModel> getAllRestaurant();
+}
