78c1e2b547f21ec7e0078a73586e544f2a5baaed
reynmraw <33017954+reynmraw@users.noreply.github.com>
2019-05-13 12:37:17 +0700

    Update penyesuaian database

--- RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Menu.java
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Menu.java
@@ -1,22 +1,27 @@
 package com.pbkk.fp.RestaurantService.entity;
 
+import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
 @Table(name="menu")
-public class Menu extends Restaurant{
-	//@Id
-	//@GeneratedValue(strategy=GenerationType.IDENTITY)
-	//private Long id;
+public class Menu implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
 	//@Embeddable
-	@ManyToOne
 	//@JoinColumn(name="restaurant", referencedColumnName="id")
 	
-	private Restaurant restaurant;
 	private String namaMakan;
 	private String deskripsi;
-	private String alamat;
-	private String nama;
+	@ManyToOne
+	private Restaurant restaurant;
+	//private String alamat;
+	//private String nama;
 	
 	public String getNamaMakan() {
 		return namaMakan;
@@ -36,13 +41,13 @@ public class Menu extends Restaurant{
 	public void setDeskripsi(String deskripsi) {
 		this.deskripsi = deskripsi;
 	}
-	/*public Long getId() {
+	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
-	}*/
-	public String getAlamat() {
+	}
+	/*public String getAlamat() {
 		return alamat;
 	}
 	public void setAlamat(String alamat) {
@@ -53,5 +58,5 @@ public class Menu extends Restaurant{
 	}
 	public void setNama(String nama) {
 		this.nama = nama;
-	}
+	}*/
 }
