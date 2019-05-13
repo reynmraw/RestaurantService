c5aec1b721e5bec202534d88f9ade4ac0e17154a
reynmraw <33017954+reynmraw@users.noreply.github.com>
2019-05-14 06:28:09 +0700

    Update penyesuaian attr

--- RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Restaurant.java
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Restaurant.java
@@ -6,17 +6,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="restaurant")
 public class Restaurant implements Serializable{
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	@JoinTable(name="list",
+				joinColumns=@JoinColumn(name="id"),
+				inverseJoinColumns=@JoinColumn(name="restaurant"))
+	//private Menu menu;
 	private Long id;
-	
 	private String nama;
 	private String alamat;
+	private Integer noTelp;
+	private String email;
+	private Float latitude;
+	private Float longitude;
 	
 	public String getNama() {
 		return nama;
@@ -36,5 +47,34 @@ public class Restaurant implements Serializable{
 	public void setAlamat(String alamat) {
 		this.alamat = alamat;
 	}
-	
+	/*public Menu getMenu() {
+		return menu;
+	}
+	public void setMenu(Menu menu) {
+		this.menu = menu;
+	}*/
+	public Integer getNoTelp() {
+		return noTelp;
+	}
+	public void setNoTlp(Integer noTelp) {
+		this.noTelp = noTelp;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Float longitude) {
+		this.longitude = longitude;
+	}
+	public Float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Float latitude) {
+		this.latitude = latitude;
+	}
 }
