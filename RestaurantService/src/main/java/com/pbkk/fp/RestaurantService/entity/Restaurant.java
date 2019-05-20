7aa6514ef095d1ec6cda77afa90072262eceed49
reynmraw <33017954+reynmraw@users.noreply.github.com>
2019-05-21 06:17:23 +0700

    Update Restaurant.java

--- RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Restaurant.java
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/entity/Restaurant.java
@@ -64,7 +64,7 @@ public class Restaurant implements Serializable{
 	public String getAlamat() {
 		return alamat;
 	}
-	public void setAlamat(String Alamat) {
+	public void setAlamat(String alamat) {
 		this.alamat = alamat;
 	}
 	public String getNoTelp() {
