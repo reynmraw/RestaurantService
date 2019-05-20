545a69953ec7fff57107180c352c3a573030a043
Ahmad Imam Fadhila <ahmadimamfadhila@gmail.com>
2019-05-21 04:02:28 +0700

    Auto stash before merge of "master" and "origin/master"

--- RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/connect/GetRequest.java
+++ RestaurantService/src/main/java/com/pbkk/fp/RestaurantService/connect/GetRequest.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,7 +18,7 @@ import com.mysql.cj.xdevapi.JsonArray;
 public class GetRequest {
 	public static void main(String[] args) {
 		 try {
-		   String access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJfdHlwZSI6InVzZXIiLCJ1c2VyX25hbWUiOiJjdXN0b21lciIsInNjb3BlIjpbInJlYWRfcmVzdGF1cmFudCJdLCJleHAiOjE1NTg0NTY2MTYsImF1dGhvcml0aWVzIjpbIlVzZXIiXSwianRpIjoiNGMzY2FjZTYtZDExNS00OWRkLTk0OTktZmMzYWFmMjk2ZmFlIiwiY2xpZW50X2lkIjoicmVzdGF1cmFudCJ9.bTsw4udwkBhWseUQ0rcYViJlS7HulCyBRS5tnwSTZLk";
+		   String access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJfdHlwZSI6InVzZXIiLCJ1c2VyX25hbWUiOiJjdXN0b21lciIsInNjb3BlIjpbInJlYWRfcmVzdGF1cmFudCJdLCJleHAiOjE1NTg0NzIzODcsImF1dGhvcml0aWVzIjpbIlVzZXIiXSwianRpIjoiMGFkNTgzYzQtNmRkNS00MmY0LWExYWItMzAyMzA4MzU2Y2FhIiwiY2xpZW50X2lkIjoicmVzdGF1cmFudCJ9.B9T2-cgSWqdAkIfak57ne23WfpuhxkhzXDHff_zEdZw";
 		   String url = "https://rendoru.com/kuliah/pbkk/restaurants";
 		   URL obj = new URL(url);
 		   HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -57,17 +62,22 @@ public class GetRequest {
 			e.printStackTrace();
 		}*/
 		   
-		   
+		   //List listID = new ArrayList();
+		   Map<Integer, String> mapResto = new HashMap<>();
 			
 			  JSONArray jsonArray = new JSONArray(response.toString()); 
 			  for (int i = 0; i < jsonArray.length(); i++) { 
 				  if (jsonArray.get(i) instanceof JSONObject) {
 					  JSONObject jsonObj = (JSONObject)jsonArray.get(i);
 					  Integer idresto = (Integer)jsonObj.get("id");
-					  System.out.println(idresto);
-					  System.out.println((String)jsonObj.get("nama")); 
+					  String namaresto = (String)jsonObj.get("nama");
+					  //listID.add(idresto);
+					  mapResto.put(idresto, namaresto);
+					  //System.out.println(idresto);
+					  //System.out.println(namaresto); 
 					  } 
 				  }
+			  System.out.println(Arrays.asList(mapResto));
 			 
 	   
 	   /*JSONObject jsonObj = new JSONObject(response.toString());
